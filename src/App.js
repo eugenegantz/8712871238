b4712b3638e7230dc8863cd27a2aa07ebc41d2b3
eg <eostapenko@vwell.su>
2023-03-15 22:13:07 +0300

    void

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 const TRANSITION_TIME_BALL = 2000;
 const COOLDOWN_TIME = 5000;
@@ -8,56 +8,19 @@ const COOLDOWN_TIME = 5000;
 function App() {
 	let [cooldownClock, setCooldownClock] = useState(0);
 
-	let refBlockLeft = useRef();
-	let ref3 = useRef();
-	let refBall = useRef();
+	let refBlockLeft = useRef(); // Левый блок
+	let refOuter = useRef();     // Контейнер с блоками и кругом
+	let refBall = useRef();      // Шар
+
+	// Постоянные таймеры
 	let timers = useRef({
 		cooldown: null,
 		trans1: null,
 		trans2: null,
 	});
 
-	function onClickStart() {
-		transition();
-	}
-
-	function initialStyles() {
-		refBall.current.style.visibility = 'hidden';
-		refBall.current.style.transition = void 0;
-		refBall.current.style.top = 0;
-		refBall.current.style.left = 0;
-	};
-
-	function transition() {
-		clearTimeout(timers.current.trans1);
-		clearTimeout(timers.current.trans2);
-
-		initialStyles();
-
-		// Установить обратный отсчет
-		cooldown(COOLDOWN_TIME);
-
-		let top = refBlockLeft.current.offsetTop / ref3.current.clientHeight * 100 + '%';
-
-		// Установить стартовое место для круга
-		refBall.current.style.visibility = 'visible';
-		refBall.current.style.top = top;
-		refBall.current.style.left = '0%';
-
-		// Установить пункт назначения анимации
-		timers.current.trans1 = setTimeout(() => {
-			refBall.current.style.transition = TRANSITION_TIME_BALL / 1000 + 's';
-			refBall.current.style.top = '50%';
-			refBall.current.style.left = '100%';
-		}, 0);
-
-		// Завершить анимацию круга через время TRANSITION_TIME_BALL
-		timers.current.trans2 = setTimeout(() => {
-			initialStyles();
-		}, TRANSITION_TIME_BALL);
-	};
-
-	function cooldown(t) {
+	// Обратный отсчет
+	const cooldown = useCallback(function _cooldown(t) {
 		clearInterval(timers.current.cooldown);
 
 		setCooldownClock(t / 1000);
@@ -72,14 +35,73 @@ function App() {
 				return t - 1;
 			});
 		}, 1000);
-	}
+	}, []);
+
+	const onClickStart = useCallback(function _onClickStart() {
+		transition();
+	});
+
+	// Сбросить состояние шара до первичных
+	const initialStyles = useCallback(function _initialStyles() {
+		refBall.current.style.visibility = 'hidden';
+		refBall.current.style.transition = void 0;
+		refBall.current.style['transition-timing-function'] = void 0;
+		refBall.current.style.top = 0;
+		refBall.current.style.left = 0;
+	}, []);
+
+	// Решение в лоб -- прямая мутация стилей через ref
+	// 
+	// Можно менять стили и через состояние,
+	// но это сложнее в реализации
+	// и вычислительно более затратная операция
+	// 
+	// На мой взгляд, к ней имеет смысл прибегать, когда реализуется мультиплатформенный компонент
+	// или компонент устроен сложнее чем HTMLElement
+
+	const transition = useCallback(function _transition() {
+		// Рассчитать положение блока в моменте (относительно, в процентах)
+		// Установить стартовую точку круга в соотв с текущим положением блока
+		// Установить "режим анимации" и задать конечные координаты => круг приступает к анимации изменения координат
+		// По истечению времени анимации -- сбросить состояние круга до первичных
+
+		clearTimeout(timers.current.trans1);
+		clearTimeout(timers.current.trans2);
+
+		initialStyles();
+
+		// Установить обратный отсчет
+		cooldown(COOLDOWN_TIME);
+
+		// Рассчитать положение блока в моменте (относительно контейнера, в процентах)
+		let top = refBlockLeft.current.offsetTop / refOuter.current.clientHeight * 100 + '%';
+
+		// Установить стартовую точку для круга
+		refBall.current.style.visibility = 'visible';
+		refBall.current.style.top = top;
+		refBall.current.style.left = '0%';
+
+		// Установить конечные координаты и анимацию
+		timers.current.trans1 = setTimeout(() => {
+			refBall.current.style.transition = TRANSITION_TIME_BALL / 1000 + 's';
+			refBall.current.style['transition-timing-function'] = 'linear';
+			refBall.current.style.top = '50%';
+			refBall.current.style.left = '100%';
+		}, 0);
+
+		// Завершить анимацию круга через время TRANSITION_TIME_BALL
+		// Сбросить состояние
+		timers.current.trans2 = setTimeout(() => {
+			initialStyles();
+		}, TRANSITION_TIME_BALL);
+	}, []);
 
 	return (
 		<div>
-			<div className="outer" ref={ref3}>
+			<div className="outer" ref={refOuter}>
 				<CompBlock side="left" _ref={refBlockLeft} />
 				<CompBlock side="right" />
-				<Ball _ref={refBall} />
+				<CompBall _ref={refBall} />
 				<CompButton onClick={onClickStart} disabled={cooldownClock}>
 					start {cooldownClock}
 				</CompButton>
@@ -88,30 +110,37 @@ function App() {
 	);
 }
 
-function Ball(props) {
-	let roundClassName = 'ball';
-
-	if (props.isAnimated) {
-		roundClassName += ' animated';
-	}
-
-	return <div className={roundClassName} ref={props._ref} />
+function CompBall(props) {
+	return (
+		<div
+			className="ball"
+			ref={props._ref}
+		/>
+	);
 }
 
 function CompBlock(props) {
-	return <div className={'block ' + props.side} ref={props._ref}></div>
+	return (
+		<div
+			className={'block ' + props.side}
+			ref={props._ref}
+		/>
+	);
 }
 
 function CompButton(props) {
-	let buttonProps = {
-		disabled: props.disabled || void 0
+	let _props = {
+		disabled: props.disabled || void 0,
 	};
 
-	return <button onClick={props.onClick} {...buttonProps}>
-		{
-			props.children
-		}
-	</button>
+	return (
+		<button
+			onClick={props.onClick}
+			{..._props}
+		>
+			{props.children}
+		</button>
+	);
 }
 
 export default App;
